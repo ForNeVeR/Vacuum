16dc3398a7766c7d1918080f0651a51eddb491f3
Friedrich von Never <friedrich@fornever.me>
2016-01-24 11:14:14 +0600

    Program: statistics logging

---
+++ Vacuum/CleanResult.fs
@@ -0,0 +1,14 @@
+﻿namespace Vacuum
+
+open System
+
+type RemoveStatus = Ok | Error
+
+type CleanResult =
+    { Directory : string
+      CleanedDate : DateTime
+      ItemsBefore : int
+      ItemsAfter : int
+      States : Map<RemoveStatus, int>
+      TimeTaken : TimeSpan }
+
--- Vacuum/Program.fs
+++ Vacuum/Program.fs
@@ -1,9 +1,10 @@
-﻿open System
+﻿module Vacuum.Program
+
+open System
+open System.Diagnostics
 open System.IO
 
 open Microsoft.VisualBasic.FileIO
-
-type RemoveResult = Ok | Error
 
 let printColor color (message : string) =
     let oldColor = Console.ForegroundColor
@@ -11,16 +12,19 @@ let printColor color (message : string) =
     Console.WriteLine message
     Console.ForegroundColor <- oldColor
 
-let info = printColor ConsoleColor.White
-let ok () = printColor ConsoleColor.Green "ok"
-let error = printColor ConsoleColor.Red
+let private info = printColor ConsoleColor.White
+let private ok () = printColor ConsoleColor.Green "ok"
+let private error = printColor ConsoleColor.Red
+
+let private itemCount path =
+    (Directory.GetFileSystemEntries path).Length
 
 let lastTouchedEarlierThan date path =
     File.GetCreationTimeUtc path < date
     && File.GetLastAccessTimeUtc path < date
     && File.GetLastWriteTimeUtc path < date
 
-let needToRemoveTopLevel date path =
+let private needToRemoveTopLevel date path =
     try
         if File.Exists path then
             lastTouchedEarlierThan date path
@@ -28,48 +32,72 @@ let needToRemoveTopLevel date path =
             Directory.EnumerateFileSystemEntries (path, "*.*", SearchOption.AllDirectories)
             |> Seq.forall (lastTouchedEarlierThan date)
     with
-    | :? UnauthorizedAccessException -> false        
+    | :? UnauthorizedAccessException -> false
 
-let remove item =
+let private remove item =
     try
         if File.Exists item then
             printf "Removing file %s... " item
             FileSystem.DeleteFile (item, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin)
-        else 
+        else
             printf "Removing directory %s... " item
             FileSystem.DeleteDirectory (item, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin)
 
         ok ()
 
         Ok
-    with 
+    with
     | ex ->
         error (ex.Message)
         Console.Error.WriteLine (ex.ToString ())
-        
+
         Error
 
-let clean directory =
+let private clean directory =
+    let stopwatch = Stopwatch ()
+    stopwatch.Start ()
+
     info (sprintf "Cleaning directory %s" directory)
 
+    let itemsBefore = itemCount directory
     let date = DateTime.UtcNow.AddMonths -1
 
-    Directory.EnumerateFileSystemEntries directory
-    |> Seq.filter (needToRemoveTopLevel date)
-    |> Seq.map remove
-    |> Seq.groupBy id
-    |> Seq.map (fun (key, s) -> (key, Seq.length s))
-    |> Map.ofSeq
+    let states =
+        Directory.EnumerateFileSystemEntries directory
+        |> Seq.filter (needToRemoveTopLevel date)
+        |> Seq.map remove
+        |> Seq.groupBy id
+        |> Seq.map (fun (key, s) -> (key, Seq.length s))
+        |> Map.ofSeq
+
+    let itemsAfter = itemCount directory
+    let time = stopwatch.Elapsed
+
+    { Directory = directory
+      CleanedDate = date
+      ItemsBefore = itemsBefore
+      ItemsAfter = itemsAfter
+      States = states
+      TimeTaken = time }
 
 [<EntryPoint>]
-let main _ = 
+let main _ =
     let directory = Path.GetTempPath ()
-    let results = clean directory
-    let getResult r = defaultArg (Map.tryFind r results) 0
+    let result = clean directory
+
+    info (sprintf "\nDirectory %s cleaned up" result.Directory)
+    info (sprintf "  Cleaned up files older than %s" (result.CleanedDate.ToString "s"))
+
+    info (sprintf "\n  Items before cleanup: %d" result.ItemsBefore)
+    info (sprintf "  Items after cleanup: %d" result.ItemsAfter)
+
+    let getResult r = defaultArg (Map.tryFind r result.States) 0
     let successes = getResult Ok
     let errors = getResult Error
 
-    printColor ConsoleColor.Green (sprintf "\nFinished ok: %d" successes)
-    printColor ConsoleColor.Red (sprintf "Finished with error: %d" errors)
+    printColor ConsoleColor.Green (sprintf "\n  Finished ok: %d" successes)
+    printColor ConsoleColor.Red (sprintf "  Finished with error: %d" errors)
+
+    info (sprintf "\n  Total time taken: %A" result.TimeTaken)
 
     0
