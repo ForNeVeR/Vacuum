ae2f51eeb2073cfbf686f84ca9d736782745477f
Friedrich von Never <friedrich@fornever.me>
2016-02-09 21:02:55 +0600

    Project: release 0.3

--- Vacuum/AssemblyInfo.fs
+++ Vacuum/AssemblyInfo.fs
@@ -8,6 +8,6 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyCopyright("Copyright © 2016 F. von Never")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("c41a92b8-6e18-4e29-b3de-849e1ea1ad46")>]
-[<assembly: AssemblyVersion("0.2.0.0")>]
+[<assembly: AssemblyVersion("0.3.0.0")>]
 
 ()
