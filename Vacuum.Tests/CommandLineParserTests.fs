e2fa5131f5234a514b333f90cfdf1d22b2efce62
Friedrich von Never <friedrich@fornever.me>
2016-04-16 15:53:13 +0700

    Commands: add Space parameter

--- Vacuum.Tests/CommandLineParserTests.fs
+++ Vacuum.Tests/CommandLineParserTests.fs
@@ -4,9 +4,25 @@ open Xunit
 
 open Vacuum
 
+let parseCommandLine = CommandLineParser.parse >> Option.get
+
 [<Fact>]
 let ``--directory parameter should be parsed`` () =
     let directory = "C:\\Nonexistent"
     let args = [| "--directory"; directory |]
-    let options = CommandLineParser.parse args |> Option.get
+    let options = parseCommandLine args
     Assert.Equal (Some directory, options.Directory)
+
+[<Fact>]
+let ``--space parameter should be parsed with postfix`` () =
+    let test text result =
+        let args = [| "--space"; text |]
+        let options = parseCommandLine args
+        Assert.Equal (result, Option.get options.BytesToFree)
+
+    let k = 1024
+    let M = k * 1024
+    test "10" 10
+    test "10k" (10 * k)
+    test "10m" (10 * M)
+    test "10M" (10 * M)
--- Vacuum/Commands.fs
+++ Vacuum/Commands.fs
@@ -12,4 +12,17 @@ type Clean =
       [<Option('p',
                "period",
                HelpText = "Number of days for item to stay untouched before being cleaned up. Default period is 30 days.")>]
-      Period : int option }
+      Period : int option
+
+      [<Option('s',
+               "space",
+               HelpText = "Amount of space to be freed disregard the dates. Off by default. Supports k and m postfix. For example, 10k = 10 kibibytes, 10m = 10 mebibytes")>]
+      Space : string option }
+
+    member this.BytesToFree =
+        let allButLast (s : string) = s.Substring (0, s.Length - 1)
+        match this.Space with
+        | Some space when space.ToLowerInvariant().EndsWith("k") -> Some (1024 * (int <| allButLast space))
+        | Some space when space.ToLowerInvariant().EndsWith("m") -> Some (1024 * 1024 * (int <| allButLast space))
+        | Some space -> Some (int space)
+        | None -> None
