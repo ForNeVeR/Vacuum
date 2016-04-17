2757c1fe2af2736033cefa6316e240642212737a
Friedrich von Never <friedrich@fornever.me>
2016-04-17 17:32:20 +0700

    CleanTests: update tests

--- Vacuum.Tests/CleanTests.fs
+++ Vacuum.Tests/CleanTests.fs
@@ -13,5 +13,5 @@ let ``Cleaner should always clean the bytes it told to`` () =
     use directory = prepareEnvironment [ { Name = "file0.txt"; Date = DateTime (2010, 1, 1); Size = 10L * M }
                                          { Name = "file1.txt"; Date = DateTime (2011, 1, 1); Size = 10L * M }
                                          { Name = "file2.txt"; Date = DateTime (2011, 1, 1); Size = 10L * M } ]
-    let result = Program.clean directory.Path (DateTime (2010, 1, 1)) (Some <| 15L * M)
-    Assert.Equal ([ "file2.txt" ], directory.GetFiles())
+    ignore <| Program.clean directory.Path (DateTime (2012, 1, 1)) (Some <| 15L * M)
+    Assert.Equal<string> ([| "file2.txt" |], directory.GetFiles())
--- Vacuum.Tests/Utils.fs
+++ Vacuum.Tests/Utils.fs
@@ -10,22 +10,26 @@ type FileInfo =
 
 type DisposableDirectory =
     { Path : string }
-    member this.GetFiles () : string seq =
-        Directory.EnumerateFileSystemEntries this.Path
+    member this.GetFiles () : string[] =
+        Directory.GetFileSystemEntries this.Path
     interface IDisposable with
         member this.Dispose () =
             Directory.Delete (this.Path, true)
 
-let private setLastChange path date =
-    [ File.SetCreationTimeUtc; File.SetLastAccessTimeUtc; File.SetLastWriteTimeUtc ]
-    |> Seq.iter (fun f -> f (path, date))
+let private setDate setters args = setters |> Seq.iter ((|>) args)
+
+let private setFileDate =
+    setDate [ File.SetCreationTimeUtc; File.SetLastAccessTimeUtc; File.SetLastWriteTimeUtc ]
+
+let private setDirectoryDate =
+    setDate [ Directory.SetCreationTimeUtc; Directory.SetLastAccessTimeUtc; Directory.SetLastWriteTimeUtc ]
 
 let private createFile location file =
     let path = Path.Combine (location, file.Name)
     do
         use stream = File.Create path
         stream.SetLength file.Size
-    setLastChange path file.Date
+    setFileDate (path, file.Date)
 
 let prepareEnvironment (infos : FileInfo seq) : DisposableDirectory =
     let infos' = Seq.cache infos
@@ -38,9 +42,8 @@ let prepareEnvironment (infos : FileInfo seq) : DisposableDirectory =
     let path = Path.Combine (Path.GetTempPath (), name)
 
     ignore <| Directory.CreateDirectory path
-    setLastChange path minDate
+    setDirectoryDate (path, minDate)
 
     infos' |> Seq.iter (createFile path)
 
     { Path = path }
-
