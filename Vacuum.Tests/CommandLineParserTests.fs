69f1ffa96cadfbcc71bddae15ab262696f952670
gustav-langer <ich.leake.nicht@meine.email.adresse>
2023-10-05 20:43:44 +0200

    Added tests for parsing

--- Vacuum.Tests/CommandLineParserTests.fs
+++ Vacuum.Tests/CommandLineParserTests.fs
@@ -28,6 +28,20 @@ let ``--space parameter should be parsed with postfix`` () =
     test "10M" (10L * M)
 
 [<Fact>]
+let ``--free parameter should be parsed with postfix`` () =
+    let test text result =
+        let args = [| "--free"; text |]
+        let options = parseCommandLine args
+        Assert.Equal (result, Option.get options.FreeUntil)
+
+    let k = 1024L
+    let M = k * 1024L
+    test "10" 10L
+    test "10k" (10L * k)
+    test "10m" (10L * M)
+    test "10M" (10L * M)
+
+[<Fact>]
 let ``--force parameter should be parsed``(): unit =
     Assert.False((parseCommandLine Array.empty).Force)
     Assert.True((parseCommandLine [|"--force"|]).Force)
